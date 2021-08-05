cb20071de98e52e05f6fadbe57b8133cd6db4a9e
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2021-08-05 17:18:23 +0530

    Update Assignment7.java

--- String/Assignment7.java
+++ String/Assignment7.java
@@ -1,3 +1,13 @@
+/*Write a java program that will concatenate 2 strings and return the result. 
+The result should be in lowercase.
+Note:If the concatenation creates a double-char, then one of the characters need to be omitted.
+Example1)
+i/p:Sachin,Tendulkar
+o/p:sachin tendulkar
+Example2)
+i/p:Mark,kate
+o/p:markate
+*/
 
 import java.util.Scanner;
 public class MyClass {
@@ -6,8 +16,9 @@ public class MyClass {
     Scanner obj=new Scanner(System.in);
     System.out.println("Enter the string1");
     String str=obj.nextLine();
-    if(str.charAt(0).equals("x") && str.charAt(str.length()).equals("x"))
-    System.out.println(str.substring(1,(str.length-1)));
+    int n=str.length();
+    if(((str.charAt(0)) == 'x') && (str.charAt(n-1))=='x')
+    System.out.println(str.substring(1,(n-1)));
     else
     System.out.println(str);
     }
