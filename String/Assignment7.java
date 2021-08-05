372b95d3d6fa2eb4883dba79098838409088bdcf
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2021-08-05 16:27:41 +0530

    Update Assignment7.java

--- String/Assignment7.java
+++ String/Assignment7.java
@@ -1,1 +1,14 @@
 
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) 
+    {
+    Scanner obj=new Scanner(System.in);
+    System.out.println("Enter the string1");
+    String str=obj.nextLine();
+    if(str.charAt(0).equals("x") && str.charAt(str.length()).equals("x"))
+    System.out.println(str.substring(1,(str.length-1)));
+    else
+    System.out.println(str);
+    }
+}
