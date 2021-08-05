f26fc901aaad0a82520c1b23be8d41d7d55092a9
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2021-08-05 15:36:40 +0530

    Update Assignment 2.java

--- String/Assignment 2.java
+++ String/Assignment 2.java
@@ -1,1 +1,16 @@
+// //concat 2 string and change to lower case
 
+import java.util.Scanner;
+public class MyClass {
+    public static void main(String args[]) 
+    {
+    Scanner obj=new Scanner(System.in);
+    System.out.println("Enter the first string");
+    String str=obj.nextLine();
+    StringBuffer sb= new StringBuffer(str);
+    System.out.println("Enter the Next String");
+    String str1=obj.nextLine();
+    sb.append(str1);
+    System.out.println("The Final String is "+sb);
+    }
+}
