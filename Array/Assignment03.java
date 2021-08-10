e9efc13b11dba5fc66f5b83c142baa21165314dc
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2021-08-10 13:45:21 +0530

    Update Assignment03.java

--- Array/Assignment03.java
+++ Array/Assignment03.java
@@ -4,18 +4,12 @@ If the number is not found, it will print -1 else it will print the index value 
 Example 1) If the Array elements are  {1,4,34,56,7} and the search element is 90, then the output expected is -1.
 Example 2)If the Array elements are  {1,4,34,56,7} and the search element is 56, then the output expected is 3.
 */
-  
-/*Write a program to initialize an integer array with values and check if a given number is present in the array or not.
-If the number is not found, it will print -1 else it will print the index value of the given  number in the array.
-Example 1) If the Array elements are  {1,4,34,56,7} and the search element is 90, then the output expected is -1.
-Example 2)If the Array elements are  {1,4,34,56,7} and the search element is 56, then the output expected is 3.
-*/
 import java.util.*;
 public class ArrayDemo
 {
 public static void main(String [] args)
 {
-    int n=0,num=0;
+    int n=0,num=0,flag=0,pos=0;
     Scanner sc=new Scanner(System.in);
     System.out.println("Enter the number of elements");
     n=sc.nextInt();
@@ -29,12 +23,13 @@ public static void main(String [] args)
     for(int i=0;i<n;i++)
     {
         if(num == ary[i])
-        System.out.println("Item Found "+ (i+1));
-        
+        {flag=1;pos=i+1;
+        break;}
     }
-    System.out.println("Output array");
-    for(int i:ary)
-    System.out.println(i);
-    System.out.println("max "+ max + "\tmin is "+ min);
+    if(flag == 0)
+    System.out.println("-1");
+    if(flag==1)
+    System.out.println("Item Found At"+ pos );
+    
 }
 }
