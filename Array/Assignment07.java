f084b2e32c750cd2f59cdb36a7a4756f025b6500
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2021-08-11 10:04:22 +0530

    Update Assignment07.java

--- Array/Assignment07.java
+++ Array/Assignment07.java
@@ -3,5 +3,29 @@ Example)
 I/P:{12,34,12,45,67,89}
 O/P:{12,34,45,67,89}
 
-Using Treemap 
+Using Hashset 
 */
+import java.util.*;
+public class Duplicate
+{
+    public static void main (String[] args)
+    {
+        System.out.println("Enter the number of elements");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Enter the elements");
+        int[]array=new int[n];
+        for(int i=0;i<n;i++)
+        {
+            array[i]=sc.nextInt();
+        }
+        System.out.println("New Array without Duplicate");
+        HashSet<Integer> hs= new HashSet();
+        for(int i:array)
+        hs.add(i);
+        Iterator<Integer> i=hs.iterator();
+        while(i.hasNext())
+        System.out.println(i.next());
+        
+    }
+}
