b7031f2f6fbb4d4072305cdf837ba4bc2eff2e8e
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2021-08-11 09:47:32 +0530

    Update Assignment06.java

--- Array/Assignment06.java
+++ Array/Assignment06.java
@@ -8,7 +8,7 @@ public class duplicate
 {
     public static void main(String [] args)
     {
-        /*
+        
         System.out.println("Enter the number of elements");
         Scanner sc=new Scanner(System.in);
         int n =sc.nextInt();
@@ -19,12 +19,10 @@ public class duplicate
         {
             ary[i]=sc.nextInt();
         }
-        int[] temp= new int[n];
-        *///create dummy array
-        int arr[]={1,2,3,4,5,4,3,2};
-        int m=0;
-        for(int i=0;i<arr.length;i++)
-        m=Math.max(m,arr[i]);
+        //create dummy array
+        int m=0;//m for max value of
+        for(int i=0;i<n;i++)
+        m=Math.max(m,ary[i]);
         //
         int temp[]=new int[m+1];
         for(int i=0;i<temp.length;i++)
@@ -32,15 +30,12 @@ public class duplicate
              temp[i]=0;
         }
         //Counting elements 
-        System.out.println("Temp Array ");
         // temp array
-        for(int i:temp)
-        System.out.println(i);
-        /*
-        for(int i=0;i<arr.length;i++)
+        for(int i=0;i<n;i++)
         {
-             temp[arr[i]]++;
+            temp[ary[i]]++;
         }
+        
         System.out.println("New Array ");
         // temp(element positon  value inc)
         //printing final array
@@ -48,7 +43,7 @@ public class duplicate
         {
              if(temp[i]>0)
              System.out.println(i);
-        }*/
+        }
         
     }
 }
