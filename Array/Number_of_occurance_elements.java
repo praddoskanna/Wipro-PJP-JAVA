e8b639c9c1bb75bb7b049321214ffab093bd8e3e
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2021-08-11 16:40:16 +0530

    Update Number_of_occurance_elements.java

--- Array/Number_of_occurance_elements.java
+++ Array/Number_of_occurance_elements.java
@@ -9,4 +9,29 @@ op
 4-1
 5-1
 */
+import java.util.*;
+
+public class reversearray
+{
+    public static void main(String [] args)
+    {   
+        System.out.println("Enter the number of elements");
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        System.out.println("Enter the elements");
+        int[]array=new int[n];
+        for(int i=0;i<n;i++)
+            array[i]=sc.nextInt();
+        ArrayList<Integer> ar=new ArrayList();// to count the occurance
+        HashSet<Integer>hr=new HashSet();// to make the input array without duplicate
+        for(int i:array)
+        {ar.add(i);
+        hr.add(i);}
+        ArrayList<Integer> ls=new ArrayList<Integer>(hr);//to display elements
+        for(int i=0;i<ls.size();i++)
+        {   
+            System.out.println("The occurance of element "+ ls.get(i) +" is  "+ Collections.frequency(ar,ls.get(i)));
+        }
+    }
+}
 
