88860c19b33483a316d8410b0bdc4fd66113c5a6
Praddos Kanna <69679608+praddoskanna@users.noreply.github.com>
2021-08-12 11:45:33 +0530

    Update Assignment_01.java

--- String/Assignment_01.java
+++ String/Assignment_01.java
@@ -7,11 +7,11 @@ public class MyClass {
     Scanner obj=new Scanner(System.in);
     System.out.println("Enter the string for palindrome check");
     String str=obj.nextLine();
-    StringBuffer sb =new StringBuffer(str);
-    StringBuffer sb1 =new StringBuffer(str);
+    str.toLowerCase()
+    StringBuffer sb =new StringBuffer(str.toLowerCase());
     System.out.println("String in BUFFER :" + sb);
-    sb.reverse();
-    if(sb.equals(sb1))
+    String rev=sb.reverse().toString();
+    if(str.equals(rev))
     System.out.println("The Given String is a Palindrome");
     else 
     System.out.println("The Given String is not a Palindrome");
